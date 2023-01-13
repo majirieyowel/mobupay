f64634b83f68699a8494821b3089ebdc3988c7bc
Majiri Eyowel <majiri@uniswitchng.com>
2023-01-13 03:11:11 +0100

    WIP account security

---
+++ mobupay/pages/secure-whatsapp-device/_token.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-row class="content px-md-5">
+    <v-col class="pa-md-0" cols="12" sm="6"> Instructions: </v-col>
+  </v-row>
+</template>
+
+<script>
+import errorCatch from "../../functions/catchError";
+
+export default {
+  name: "secure-device",
+  data: () => ({
+    token: "",
+    message: "",
+  }),
+  methods: {
+    // async verify_email() {
+    //   try {
+    //     const response = await this.$axios.post("/verify-whatsapp-email", {
+    //       token: this.token,
+    //     });
+    //     let data = response.data;
+    //     if (data.confirmed) {
+    //       this.message = "Your email has been verified successfully";
+    //     } else {
+    //       this.message = "Unable to verify email";
+    //     }
+    //   } catch (error) {
+    //     errorCatch(error, this);
+    //   }
+    // },
+  },
+  mounted() {
+    // this.token = this.$route.params.token;
+    // this.verify_email();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
